d3f954191aef369b1915fa65f12301277b7ccb72
UgolUgol <trubchenkon@gmail.com>
2017-11-30 17:56:33 +0300

    new version

---
+++ Assets/CameraScript.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+    public float speed = 1;
+    public float horizontal_speed = 1;
+    public float vertical_speed = 1;
+
+    float xrot = 0;
+    float yrot = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMoving();
+        Rotation();
+    }
+
+    void PlayerMoving()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(new Vector3(0, 0, speed * Time.deltaTime));
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(new Vector3(0, 0, -speed * Time.deltaTime));
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.Translate(new Vector3(0, speed * Time.deltaTime, 0));
+            transform.Translate(new Vector3(0, -speed * Time.deltaTime, 0));
+
+        }
+    }
+
+    void Rotation()
+    {
+        xrot -= Input.GetAxis("Mouse Y") * horizontal_speed;
+        yrot += Input.GetAxis("Mouse X") * horizontal_speed;
+        transform.eulerAngles = new Vector3(xrot, yrot, 0);
+    }
+}
---
+++ Assets/Grub.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Grub : MonoBehaviour {
+
+    public float grabPower = 10.0f;
+    public float throwPower = 50;
+    private RaycastHit hit;
+    public float rayDistance = 3.0f;
+    private bool Grab = false;
+    private bool Throw = false;
+    public Transform offset;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var mainCamera = FindCamera();
+        if (Input.GetMouseButtonDown(1))
+        {
+            Physics.Raycast(mainCamera.ScreenPointToRay(Input.mousePosition).origin, 
+                mainCamera.ScreenPointToRay(Input.mousePosition).direction, 
+                out hit, rayDistance);
+            if (hit.rigidbody)
+            {
+                Grab = true;
+            }
+        }
+        if(Input.GetMouseButton(0))
+        {
+            if(Grab)
+            {
+                Grab = false;
+                Throw = true;
+            }
+        }
+        if (Grab && hit.rigidbody)
+        {
+            hit.rigidbody.velocity =  (offset.position - (hit.transform.position + hit.rigidbody.centerOfMass)) * grabPower;
+        }
+        if(Throw && hit.rigidbody)
+        {
+            hit.rigidbody.velocity = transform.forward * throwPower;
+            Throw = false;
+        }
+	}
+
+
+    private Camera FindCamera()
+    {
+        if (GetComponent<Camera>())
+        {
+            return GetComponent<Camera>();
+        }
+
+        return Camera.main;
+    }
+}
